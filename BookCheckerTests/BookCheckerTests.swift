be33980c1347ec91979d402e6c34f3b234230be3
jingjie <jingjiewang67@gmail.com>
2021-01-25 19:04:48 +0800

    Initial Commit

---
+++ BookCheckerTests/BookCheckerTests.swift
@@ -0,0 +1,33 @@
+//
+//  BookCheckerTests.swift
+//  BookCheckerTests
+//
+//  Created by MAD2 on 25/1/21.
+//
+
+import XCTest
+@testable import BookChecker
+
+class BookCheckerTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
